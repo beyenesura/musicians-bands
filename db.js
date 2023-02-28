04d3a899f6f173e7249117c525d4b746820854ab
beyenesura <123514564+beyenesura@users.noreply.github.com>
2023-02-28 12:02:57 -0500

    Update db.js

--- db.js
+++ db.js
@@ -2,6 +2,10 @@ const path = require('path');
 const { Sequelize, Model } = require('sequelize');
 
 // TODO - create the new sequelize connection
+const sequelize = new Sequelize('database_name', 'username', 'password', {
+  host: 'localhost',
+  dialect: 'mysql'
+});
 
 module.exports = {
     sequelize,
