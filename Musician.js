85bb676221aa5dc7c204411d7c42a98f3f540a31
beyenesura <123514564+beyenesura@users.noreply.github.com>
2023-02-28 11:52:21 -0500

    Update Musician.js

--- Musician.js
+++ Musician.js
@@ -1,6 +1,17 @@
 const {Sequelize, sequelize} = require('./db');
 
 // TODO - define the Musician model
+const Musician = sequelize.define('Musician', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  instrument: {
+    type: Sequelize.STRING,
+    allowNull: false
+  }
+});
+
 let Musician;
 
 module.exports = {
